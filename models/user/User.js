6feeebca0a4ba0d6b8ab1d1c1fda8e9aa9d5e906
Ryan <lizkaryan626@gmail.com>
2026-02-04 17:50:15 +0300

    configuration connection

--- models/user/User.js
+++ models/user/User.js
@@ -12,4 +12,4 @@ const userSchema = new mongoose.Schema({
   notifications: [notificationSchema]
 });
 
-const User = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
