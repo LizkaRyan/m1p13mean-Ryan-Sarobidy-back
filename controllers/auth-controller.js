9b1b3258e58f7046346f1a9bbc49d423545e784d
Ryan <lizkaryan626@gmail.com>
2026-02-28 12:38:16 +0300

    signup

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -23,4 +23,23 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { login };
+const post = async function (req, res) {
+  try {
+    const { name, email, password, role } = req.body;
+    let user = new User({ name, email, password, role, favorites: [], notifications: [] });
+    user = await user.save();
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({ token, user: { _id: user._id, role: user.role.code } });
+  }
+  catch (err) {
+    res.status(500).json({ message: 'Erreur serveur', error: err.message });
+  }
+}
+
+module.exports = { login, post };
