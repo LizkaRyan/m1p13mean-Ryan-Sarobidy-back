2ca9e7e178089e4b5777f43c943e965cc119745b
sarobidyrazafy <sarobidyraza101@gmail.com>
2026-03-01 21:21:33 +0100

    Shop Router

--- routes/shop-router.js
+++ routes/shop-router.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getAllShops, getShop, save, put, patch } = require('../controllers/shop-controller');
 
-// CRUD Shop
-router.get('/', getAllShops);       // GET /api/shops
-router.get('/:id', getShop);        // GET /api/shops/:id
-router.post('/', save);             // POST /api/shops
-router.put('/:id', put);            // PUT /api/shops/:id
-router.patch('/:id', patch);        // PATCH /api/shops/:id
+router.get('/available', ShopController.getAllDisponibles);
+router.get('/:id', ShopController.getById);
+router.get('/user/:userId', ShopController.getByUserId); 
 
 module.exports = router;
